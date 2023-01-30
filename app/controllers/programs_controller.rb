be4732313c235d58cb3673d5bee1baf50677e541
Антон Науменков <NaumE64@yandex.ru>
2023-01-30 12:51:24 +0300

    seed database via query in controller

---
+++ app/controllers/programs_controller.rb
@@ -0,0 +1,26 @@
+class ProgramsController < ApplicationController
+  def fetch_programs
+    url = URI("https://dev.api.etnamed.ru/v1/graphql")
+
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+
+    request = Net::HTTP::Post.new(url)
+    request["Content-Type"] = "application/json"
+    request.body = "{\"query\":\"{\\n  dms_products {\\n created_at\\n    id\\n    medical_sum\\n    name\\n    price\\n    program\\n    uid\\n    updated_at\\n  }\\n}\",\"variables\":{}}"
+
+    response = https.request(request)
+    res = JSON.parse(response.read_body)
+    res['data']['dms_products'].each do |hash|
+      unless DmsProduct.exists?(name: hash['name'])
+        DmsProduct.create(
+          medical_sum: hash['medical_sum'],
+          name:        hash['name'],
+          price:       hash['price'],
+          program:     hash['program'],
+          uid:         hash['uid']
+        )
+      end
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require File.expand_path('../config/environment', __dir__)
+require 'telegram/bot'
+require "uri"
+require "json"
+require "net/http"
+
+ProgramsController.new.fetch_programs
